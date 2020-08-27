3ec1005514a079669f3a4698ca2586e41309c3c0
Rémy Perona <remperona@gmail.com>
2020-08-27 16:22:19 -0400

    update config

--- Tests/Unit/init-tests.php
+++ Tests/Unit/init-tests.php
@@ -3,7 +3,4 @@
  * Initializes the wp-media/phpunit handler, which then calls the rocket Unit test suite.
  */
 
-define( 'WPMEDIA_PHPUNIT_ROOT_DIR', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
-define( 'WPMEDIA_PHPUNIT_ROOT_TEST_DIR', __DIR__ );
-
 require_once WPMEDIA_PHPUNIT_ROOT_DIR . '/vendor/wp-media/phpunit/Unit/bootstrap.php';
